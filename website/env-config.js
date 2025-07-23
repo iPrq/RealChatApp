082dab48dd98798c458bcc59743de3544833c05d
iPrq <danciju@gmail.com>
2025-07-23 13:47:54 +0530

    Fix Docker image issues

--- website/env-config.js
+++
@@ -1,7 +0,0 @@
-// Frontend Environment Configuration
-// You can change these values to point to different backend URLs
-
-window.BACKEND_URL = 'https://realchatapp-1-ogou.onrender.com';
-window.APP_NAME = 'RealChatApp';
-
-console.log('🌍 Frontend configured to use backend:', window.BACKEND_URL);
